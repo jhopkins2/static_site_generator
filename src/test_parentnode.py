eed93e6e3724411dee0c1479351bd46cca26af0c
jhopkins2 <jhopkins2@islander.tamucc.edu>
2026-01-02 23:14:58 -0600

    Added ParentNode class and unit tests to test ParentNode class

---
+++ src/test_parentnode.py
@@ -0,0 +1,46 @@
+import unittest
+from parentnode import ParentNode
+from leafnode import LeafNode
+
+class TestParentNode(unittest.TestCase):
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_many_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        test_str = "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>"
+        self.assertEqual(node.to_html(), test_str)
+
+    def test_to_html_no_children(self):
+        node = ParentNode("p", [], {"href":"https://www.boot.dev"})
+        test_str = '<p href="https://www.boot.dev"></p>'
+        self.assertEqual(node.to_html(), test_str)
+
+    def test_to_html_missing_children(self):
+        node = ParentNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+
+if __name__ == "__main__":
+    unittest.main()
